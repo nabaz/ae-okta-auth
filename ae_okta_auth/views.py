69a7f80b7a42224bcb7602def2866fe726893fd4
Nabaz Maaruf <nabaz.maaruf@aenetworks.com>
2018-09-04 11:46:13 -0400

    fixed safe_url bug on view

--- ae_okta_auth/views.py
+++ ae_okta_auth/views.py
@@ -169,7 +169,7 @@ def signin(r):
         next_url = r.GET.get('next', settings.SAML2_AUTH.get('DEFAULT_NEXT_URL', 'admin:index'))
 
     # Only permit signin requests where the next_url is a safe URL
-    if not is_safe_url(next_url):
+    if not is_safe_url(next_url, None):
         return HttpResponseRedirect([denied, 'denied', 'ae_okta_auth:denied'])
 
     r.session['login_next_url'] = next_url
